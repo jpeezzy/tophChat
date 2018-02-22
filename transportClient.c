e981901cc67307347cd02e935ca870d23031cfed
khoitd1997 <khoidinhtrinh@gmail.com>
2018-02-22 01:02:41 -0800

    added basic data structure and send/receive function

--- transportClient.c
+++ transportClient.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <string.h>
+#include <errno.h>
 
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -10,23 +11,100 @@
 #include <netinet/in.h>
 #include <unistd.h>
 
+
 #include "transportClient.h"
+
+#define CHAT_SERVER_ADDR "zuma.eecs.uci.edu"
+#define RECONNECT_NUM 5
+#define CHAT_SERVER_PORT "30001"
+
+int openConnection(struct serverConnection* server)
+{
+    struct addrinfo* serverInfo;
+    struct addrinfo serverHints;
+    
+    bzero(&serverHints, sizeof(serverHints));
+
+    int count=0;
+
+    // give hints about TCP connection and IPV4
+    serverHints.ai_family=AF_INET;
+    serverHints.ai_socktype=SOCK_STREAM;
+
+    // assuming that the chat server is on zuma  
+    getaddrinfo(CHAT_SERVER_ADDR, CHAT_SERVER_PORT, &serverHints, &serverInfo);
+
+    while((connect(server->socket, serverInfo->ai_addr, &(serverInfo->ai_addrlen)))==-1 && count <RECONNECT_NUM)
+    {   
+        ++count;
+    }
+
+    // can't connect to server
+    if(count==5)
+    {
+        return -1;
+    }
+
+    // check if failed to bind 
+    freeaddrinfo(serverInfo);
+    return 0;
+}
+
+
+int closeConnection(struct serverConnection* server)
+{
+    return close(server->socket);
+}
+
+
+int sendPacket(clientPacket* packet, struct serverConnection* server)
+{
+    clientPacket* curByte=packet; 
+    int packetLeft=sizeof(packet);
+    int sent;
+
+    //implement check to make it sends all bytes
+    while(packetLeft>0)
+    {   
+
+        sent=send(server->socket, (void*) curByte, sizeof(packet),0);
+        if(sent==-1)
+        {
+            if(errno==EPIPE)
+            {
+                // the server socket has closed 
+                return -2;
+            }
+        }
+        packetLeft-=sent;
+        curByte+=sent;
+    }
+    return 0; 
+}
+
+int fetchPacket(clientPacket* packet, struct serverConnection* server)
+{
+    int temp=recv(server->socket, packet, sizeof(packet), MSG_DONTWAIT);
+    if(temp==-1)
+    {
+        if(errno==EWOULDBLOCK)
+        {   
+            //no data to read
+            return -1;
+        }
+    }
+
+    else if(temp==0)
+    {
+        // the server socket has closed 
+        return -2;
+    }
+
+    return 0;
+}
 
 int main(void)
 {   
-    struct addrinfo* serverInfo;
-    struct addrinfo hints;
-    hints.ai_family=AF_INET;
 
-    getaddrinfo("zuma.eecs.uci.edu", NULL, &hints, &serverInfo);
-
-    struct addrinfo* temp=NULL;
-    char host[100];
-    for(temp=serverInfo; temp!=NULL; temp=temp->ai_next)
-    {
-        getnameinfo(temp->ai_addr, temp->ai_addrlen, host, (socklen_t) sizeof(host), NULL,0,NI_NUMERICHOST);
-        puts(host);
-    }
-    freeaddrinfo(serverInfo);
     return 0;
 }
--- transportClient.h
+++ transportClient.h
@@ -6,6 +6,7 @@
 #include <assert.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdint.h>
 
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -14,23 +15,61 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+
 #define HOSTNAME "zuma.eecs.uci.edu"
 #define PORT "20000"
+#define MESS_LIMIT 500 // how many char user can send
+
 
 typedef struct serverConnection connection;
-
-struct serverConnection{
-    
+typedef struct messageClient clientPacket;
+typedef struct FIFObuffer fifo;
+typedef struct messageRoom chatRoom;
+// place holder for more stuffs 
+struct serverConnection
+{
     // socket used for connection
     int socket;
-
-    // linked list of possible connection
-    struct addrinfo addrList;
 };
 
-int openConnection(struct serverConnection*);
+struct messageClient
+{
+    char message[MESS_LIMIT];
+    uint8_t roomNum;
+};
 
-int closeConnection(struct serverConnection*);
+// buffer used to hold messages coming into a chat room 
+struct FIFObuffer
+{
+    char* buffer[20];
+    int curPos; 
+};
+
+struct room
+{
+    struct serverConnection server;
+    int roomNum;
+    struct FIFObuffer inMessage; 
+};
+
+int openConnection(connection* server);
+
+int closeConnection(connection* server);
+
+// place holder for interacting with room struct
+int openRoom(chatRoom* room);
+
+int closeRoom(chatRoom* room);
+
+int updateFIFO(fifo* buffer);
+
+int readBuffer(fifo* buffer);
+
+int writeBuffer(fifo* buffer);
+
+int sendPacket(clientPacket* packet, struct serverConnection* server);
+
+int fetchPacket(clientPacket* packet, struct serverConnection* server);
 
 
 #endif
