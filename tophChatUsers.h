9dd24c1f60c68d44aed84364349bd5d2a06fb2cd
jpeezzy <jpeezzy@hotmail.com>
2018-03-15 13:04:52 -0700

    tuser can now vie wfriends

--- tophChatUsers.h
+++ tophChatUsers.h
@@ -3,6 +3,7 @@
 #include "constants.h"
 
 #define ui unsigned int
+#define uli unsigned long int
 #define cp char *
 #define MAX_FRIENDS 50
 #define MAX_USERS 100
@@ -15,6 +16,7 @@ typedef struct database TINFO;
 struct tophChatUser
 {
 	cp userName;
+	ui password;
 	ui hashedPassword;
 	cp name;
 	ui hashID;
@@ -22,6 +24,7 @@ struct tophChatUser
 	cp phone;
 	TUSER *friends[MAX_FRIENDS]; //Edges to graph
 	ui friendCount;
+	uli publicKey;
 	int numOfRoomUserIn;
 	int listOfRooms[CHAT_ROOM_LIMIT];
 	int socket;
@@ -31,6 +34,8 @@ struct database
 {
 	ui numOfUsers;
 	TUSER *Users[MAX_USERS];
+	uli publicKey;
+	uli privateKey;
 };
 
 /*Creates a dataBase for data collecting  */
@@ -58,6 +63,12 @@ TUSER *checkUserByName(cp userName, TUSER *user);
 
 //List friends
 void showFriends(TUSER *user);
+
+//Lists all the friends 
+/* returns a list of the friends like this
+ * james/arvin/boostedgorilla/abel + '\0'
+ */
+int getFriends(cp username, TINFO *userbase, cp list);
 
 //Authentification gets the actual user struct
 //and checks if the password is correct
